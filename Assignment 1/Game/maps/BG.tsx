73381c59136877c7158ed9b835d7cf101c8a5e4b
VladimirAP7 <avladimirp7@gmail.com>
2017-10-12 23:36:27 +0200

    Uploading current map1

--- Assignment 1/Game/maps/BG.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="BG" tilewidth="32" tileheight="32" tilecount="1200" columns="40">
- <image source="BG.png" width="1280" height="960"/>
-</tileset>
