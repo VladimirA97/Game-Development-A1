9aaeab5b25a3802b570b82782a67ada2e4cddd2e
VladimirAP7 <avladimirp7@gmail.com>
2017-10-12 21:36:36 +0200

    Tiled

--- Assignment 1/Game/maps/BG.tsx
+++ Assignment 1/Game/maps/BG.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="BG" tilewidth="32" tileheight="32" tilecount="713" columns="31">
- <image source="../Downloads/freetileset/png/BG/BG.png" width="1000" height="750"/>
+<tileset name="BG" tilewidth="32" tileheight="32" tilecount="1200" columns="40">
+ <image source="BG.png" width="1280" height="960"/>
 </tileset>
--- Assignment 1/Game/maps/Tiles.tsx
+++ Assignment 1/Game/maps/Tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="Tile" tilewidth="32" tileheight="32" tilecount="1760" columns="40">
- <image source="../Downloads/freetileset/png/Tiles/Tile.png" width="1298" height="1411"/>
+ <image source="Tile.png" width="1298" height="1411"/>
 </tileset>
